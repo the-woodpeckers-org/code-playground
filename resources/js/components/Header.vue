5cbc0fb66f023368d5f12379110ebd19aba1fdfa
deethesaint <vtduong0912@gmail.com>
2024-09-09 22:45:22 +0700

    remake navbar

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -16,14 +16,39 @@ export default {
 </script>
 
 <template>
-    <header class="bg-amber-200">
-        <nav class="mx-auto max-w-7xl items-center justify-between h-10">
-            <h1 class="text-center text-3xl">LIVE CODE EDITOR</h1>
-        </nav>
-    </header>
-    <div class="w-full text-end">
-        <a href="#" @click="logout" class="p-1 border rounded bg-green-200" v-if="$root.auth !== null" :data="$root.auth">Logout</a>
-        <router-link class="p-1 border rounded bg-green-200" to="/login" v-if="$root.auth === null" :data="$root.auth">Login</router-link>
+    <div class="navbar bg-amber-200">
+        <div class="flex-1">
+            <router-link class="btn btn-ghost text-xl" to="/problems">Live Code Editor</router-link>
+        </div>
+        <div class="flex-none gap-2">
+            <div class="form-control">
+                <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto"/>
+            </div>
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img v-if="$root.auth !== null"
+                             alt="Avatar"
+                             src="https://phunuvietnam.mediacdn.vn/thumb_w/700/179072216278405120/2021/12/3/amee202101-16385417919281354781258-316-0-941-1000-crop-16385418365003362492.jpg"/>
+                        <img v-if="$root.auth === null" alt="Avatar"
+                             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcazeHuAcZDzv4_61fPLT-S00XnaKXch2YWQ&s">
+                    </div>
+                </div>
+                <ul
+                    tabindex="0"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    <li v-if="$root.auth !== null"><a class="text-base">Profile</a></li>
+                    <li v-if="$root.auth !== null"><a class="text-base">Settings</a></li>
+                    <li v-if="$root.auth !== null"><a class="text-base" @click="logout">Logout</a></li>
+                    <li v-if="$root.auth === null">
+                        <router-link class="text-base" to="/login">Login</router-link>
+                    </li>
+                    <li v-if="$root.auth === null">
+                        <router-link class="text-base" to="/register">Register</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
