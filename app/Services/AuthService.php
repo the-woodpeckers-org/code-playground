493d553ec319257a9bb3def7dfd10fb233e53631
deethesaint <vtduong0912@gmail.com>
2024-09-11 00:20:45 +0700

    redo register method on AuthService

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -21,17 +21,19 @@ class AuthService
 
     public function register(RegisterFormRequest $request)
     {
-        if ($request->input('password') === $request->input('confirm_password')) {
-            try {
-                $user = new User();
-                $user->name = $request->input('name');
-                $user->password = bcrypt($request->input('password'));
-                $user->email = $request->input('email');
-                $user->save();
-                return response()->json(['succeed' => true]);
-            } catch (\Exception $exception) {
-                return response()->json(['succeed' => false, 'msg' => $exception->getMessage()]);
-            }
+        try {
+            $user = new User();
+            $user->name = $request->input('name');
+            $user->password = bcrypt($request->input('password'));
+            $user->email = $request->input('email');
+            $user->address = $request->input('address');
+            $user->birthday = $request->input('birthday');
+            $user->gender = $request->input('gender');
+            $user->phone_number = $request->input('phone_number');
+            $user->save();
+            return response()->json(['succeed' => true]);
+        } catch (\Exception $exception) {
+            return response()->json(['succeed' => false, 'msg' => $exception->getMessage()]);
         }
         return response()->json(['succeed' => false]);
     }
