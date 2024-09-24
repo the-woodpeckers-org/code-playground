17c192ba9e0d3050c186e3e9e9ba2bccdd9f309d
deethesaint <vtduong0912@gmail.com>
2024-09-24 23:41:40 +0700

    transform to components

---
+++ resources/js/components/cards/Course.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "Course",
+    props: {
+        title: 'N/A',
+        imgUrl: 'N/A',
+        description: 'N/A'
+    }
+}
+</script>
+
+<template>
+    <router-link class="card bg-base-200 shadow-xl my-1 text-sm hover:bg-gray-200 hover:scale-105 duration-300 transition">
+        <figure class="h-40">
+            <img
+                :src="imgUrl"/>
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">{{ title }}</h2>
+            <p>{{ description }}</p>
+        </div>
+    </router-link>
+</template>
+
+<style scoped>
+
+</style>
