833406cd0491243ebad4c752ec771cab91307adf
deethesaint <vtduong0912@gmail.com>
2024-09-24 23:25:24 +0700

    config

--- resources/js/components/listItems/Problem.vue
+++ resources/js/components/listItems/Problem.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="grid grid-cols-6 mx-3 my-1 p-1 border-gray-300 border rounded-md h-9 bg-sky-100 font-semibold">
         <div class="ms-3 text-green-700">
-            <div class="tooltip tooltip-right" data-tip="You haven't solved this problem yet!">
-                <button><i v-if="!problem.passed_at" class="fa-regular fa-hourglass"></i></button>
+            <div v-if="!$root.auth">
+                <div class="tooltip tooltip-right"
+                     data-tip="Please login to see the status of this problem!">
+                    <button><i v-if="!problem.passed_at" class="fa-solid fa-asterisk"></i></button>
+                </div>
             </div>
-            <div class="tooltip tooltip-right" data-tip="You have solved this problem!">
-                <button><i v-if="problem.passed_at" class="fa-regular fa-circle-check"></i></button>
+            <div v-if="$root.auth">
+                <div class="tooltip tooltip-right"
+                     data-tip="You haven't solved this problem yet!">
+                    <button><i v-if="!problem.passed_at" class="fa-regular fa-hourglass"></i></button>
+                </div>
+                <div class="tooltip tooltip-right"
+                     data-tip="You have solved this problem!">
+                    <button><i v-if="problem.passed_at" class="fa-regular fa-circle-check"></i></button>
+                </div>
             </div>
+
         </div>
         <div class="col-span-2 truncate ..."><a class="w-full hover:link"
                                                 :href="'/problem/' + problem.id">{{ problem.title }}</a></div>
--- resources/js/components/pages/CodePanel.vue
+++ resources/js/components/pages/CodePanel.vue
@@ -1,105 +1,3 @@
-<template>
-    <dialog v-if="!$root.auth" id="require_logged_in_modal" class="modal modal-open">
-        <div class="modal-box">
-            <h3 class="text-lg font-bold">You must be logged in to solve this problem</h3>
-            <p class="py-4">Click login below to login, or to register if you don't have an account.</p>
-            <div class="modal-action">
-                <form method="dialog">
-                    <!-- if there is a button in form, it will close the modal -->
-                    <router-link type="button" class="btn bg-amber-300 hover:bg-amber-600" to="/login">Login now
-                    </router-link>
-                </form>
-            </div>
-        </div>
-    </dialog>
-    <dialog id="problem_solved_modal" class="modal" :class="{ 'modal-open' : isSubmitted}">
-        <div class="modal-box">
-            <h3 class="text-lg font-bold"></h3>
-            <p class="py-4"><span class="font-semibold">Congratulations! Problem solved!</span><br>Do you want to solve some other problems?</p>
-            <div class="modal-action">
-                <form method="dialog">
-                    <router-link type="button" class="btn bg-amber-300 hover:bg-amber-600 btn-sm mx-1 w-16" to="/problems">Yes</router-link>
-                    <button class="btn btn-sm w-16 bg-gray-300 hover:bg-gray-400" @click="this.isSubmitted = false">No</button>
-                </form>
-            </div>
-        </div>
-    </dialog>
-    <div class="w-full mt-2">
-
-    </div>
-    <div class="w-full mt-2">
-        <form id="formCode" method="post" action="/compile">
-            <div class="grid grid-cols-12">
-                <div class="col-span-6 me-1 border-e-2">
-                    <p class="text-2xl font-bold border-b-2 mx-1" v-if="loading"> {{ title }}</p>
-
-                    <p class="text-base mx-1" v-if="loading"> {{ description }}</p>
-                    <div class="text-2xl font-bold border-b-2 mx-1" v-if="!loading">
-                        <div class="skeleton h-8 my-1 w-full"></div>
-                    </div>
-                    <div class="text-base mx-1" v-if="!loading">
-                        <div v-for="index in 30" class="skeleton h-4 mt-1 w-full"></div>
-                    </div>
-                </div>
-                <div class="col-span-6 mx-1">
-                    <label for="language">Select language: </label>
-                    <select id="language" name="language" class="bg-gray-200 rounded-xl border-2 border-gray-400 ms-3">
-                        <option value="cpp">C++</option>
-                        <option value="c">C</option>
-                        <option value="python">Python</option>
-                        <option value="php">PHP</option>
-                        <option value="javascript">JavaScript</option>
-                    </select>
-                    <div class="h-96">
-                        <div id="editor" style="width: 100%; height: 100%" class="text-base mt-3">
-
-                        </div>
-                    </div>
-                    <div class="mt-3 w-full">
-                        <button type="button"
-                                class="rounded-xl bg-amber-300 py-1 border-gray-500 border border-amber-500 px-2 hover:bg-amber-600 w-20 h-10"
-                                @click="compile">
-                            <span v-if="!isCompiling">RUN</span>
-                            <span v-if="isCompiling" class="loading loading-dots loading-xs"></span>
-                        </button>
-                        <button id="submit-btn" type="button"
-                                class="rounded-xl bg-green-500 py-1 border-green-300 border px-2 ms-1 hover:bg-green-600 w-20 h-10 disabled:bg-gray-300 disabled:border-gray-400 disabled:text-gray-400"
-                                disabled
-                                @click="submit">
-                            <span v-if="!isSubmitting">SUBMIT</span>
-                            <span v-if="isSubmitting" class="loading loading-dots loading-xs"></span>
-                        </button>
-                        <div class="mt-3" v-if="isPassed">
-                            <p class="font-semibold text-green-600">You have already solved this problem at
-                                {{ this.passedDate }} &#128512;</p>
-                        </div>
-                    </div>
-                    <div v-if="loading" class="mt-3 w-full">
-                        Testcases passed: <span id="testcases-passed" class="font-bold">{{
-                            passedTestcases
-                        }}/{{ testcases.length }}</span>
-                    </div>
-                    <div v-if="!loading" class="mt-3 w-full">
-                        <div class="skeleton w-80 h-8"></div>
-                    </div>
-                    <div class="mt-3 w-full h-80 overflow-auto">
-                        <div v-if="!loading">
-                            <div v-for="index in 3" class="skeleton h-28 my-1 w-full"></div>
-                        </div>
-                        <div v-if="loading">
-                            <TestcaseList v-if="!run" :data="testcases"></TestcaseList>
-                            <TestcaseList v-if="run" :data="runData"></TestcaseList>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </div>
-    <div class="w-full mt-2 h-12">
-
-    </div>
-</template>
-
 <script>
 import Testcase from "@/components/listItems/Testcase.vue";
 import TestcaseList from "@/components/listItems/TestcaseList.vue";
@@ -201,3 +99,105 @@ export default {
     }
 }
 </script>
+
+<template>
+    <dialog v-if="!$root.auth" id="require_logged_in_modal" class="modal modal-open">
+        <div class="modal-box">
+            <h3 class="text-lg font-bold">You must be logged in to solve this problem</h3>
+            <p class="py-4">Click login below to login, or to register if you don't have an account.</p>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <router-link type="button" class="btn bg-amber-300 hover:bg-amber-600" to="/login">Login now
+                    </router-link>
+                </form>
+            </div>
+        </div>
+    </dialog>
+    <dialog id="problem_solved_modal" class="modal" :class="{ 'modal-open' : isSubmitted}">
+        <div class="modal-box">
+            <h3 class="text-lg font-bold"></h3>
+            <p class="py-4"><span class="font-semibold">Congratulations! Problem solved!</span><br>Do you want to solve some other problems?</p>
+            <div class="modal-action">
+                <form method="dialog">
+                    <router-link type="button" class="btn bg-amber-300 hover:bg-amber-600 btn-sm mx-1 w-16" to="/problems">Yes</router-link>
+                    <button class="btn btn-sm w-16 bg-gray-300 hover:bg-gray-400" @click="this.isSubmitted = false">No</button>
+                </form>
+            </div>
+        </div>
+    </dialog>
+    <div class="w-full mt-2">
+
+    </div>
+    <div class="w-full mt-2">
+        <form id="formCode" method="post" action="/compile">
+            <div class="grid grid-cols-12">
+                <div class="col-span-6 me-1 border-e-2">
+                    <p class="text-2xl font-bold border-b-2 mx-1" v-if="loading"> {{ title }}</p>
+
+                    <p class="text-base mx-1" v-if="loading"> {{ description }}</p>
+                    <div class="text-2xl font-bold border-b-2 mx-1" v-if="!loading">
+                        <div class="skeleton h-8 my-1 w-full"></div>
+                    </div>
+                    <div class="text-base mx-1" v-if="!loading">
+                        <div v-for="index in 30" class="skeleton h-4 mt-1 w-full"></div>
+                    </div>
+                </div>
+                <div class="col-span-6 mx-1">
+                    <label for="language">Select language: </label>
+                    <select id="language" name="language" class="bg-gray-200 rounded-xl border-2 border-gray-400 ms-3">
+                        <option value="cpp">C++</option>
+                        <option value="c">C</option>
+                        <option value="python">Python</option>
+                        <option value="php">PHP</option>
+                        <option value="javascript">JavaScript</option>
+                    </select>
+                    <div class="h-96">
+                        <div id="editor" style="width: 100%; height: 100%" class="text-base mt-3">
+
+                        </div>
+                    </div>
+                    <div class="mt-3 w-full">
+                        <button type="button"
+                                class="rounded-xl bg-amber-300 py-1 border-gray-500 border border-amber-500 px-2 hover:bg-amber-600 w-20 h-10"
+                                @click="compile">
+                            <span v-if="!isCompiling">RUN</span>
+                            <span v-if="isCompiling" class="loading loading-dots loading-xs"></span>
+                        </button>
+                        <button id="submit-btn" type="button"
+                                class="rounded-xl bg-green-500 py-1 border-green-300 border px-2 ms-1 hover:bg-green-600 w-20 h-10 disabled:bg-gray-300 disabled:border-gray-400 disabled:text-gray-400"
+                                disabled
+                                @click="submit">
+                            <span v-if="!isSubmitting">SUBMIT</span>
+                            <span v-if="isSubmitting" class="loading loading-dots loading-xs"></span>
+                        </button>
+                        <div class="mt-3" v-if="isPassed">
+                            <p class="font-semibold text-green-600">You have already solved this problem at
+                                {{ this.passedDate }} &#128512;</p>
+                        </div>
+                    </div>
+                    <div v-if="loading" class="mt-3 w-full">
+                        Testcases passed: <span id="testcases-passed" class="font-bold">{{
+                            passedTestcases
+                        }}/{{ testcases.length }}</span>
+                    </div>
+                    <div v-if="!loading" class="mt-3 w-full">
+                        <div class="skeleton w-80 h-8"></div>
+                    </div>
+                    <div class="mt-3 w-full h-80 overflow-auto">
+                        <div v-if="!loading">
+                            <div v-for="index in 3" class="skeleton h-28 my-1 w-full"></div>
+                        </div>
+                        <div v-if="loading">
+                            <TestcaseList v-if="!run" :data="testcases"></TestcaseList>
+                            <TestcaseList v-if="run" :data="runData"></TestcaseList>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="w-full mt-2 h-12">
+
+    </div>
+</template>
