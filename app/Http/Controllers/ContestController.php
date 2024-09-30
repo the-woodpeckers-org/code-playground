78c449421fb12390670479ee34c0c4e858e4bc7c
deethesaint <vtduong0912@gmail.com>
2024-09-30 22:42:58 +0700

    things

--- app/Http/Controllers/ContestController.php
+++ app/Http/Controllers/ContestController.php
@@ -20,5 +20,4 @@ class ContestController extends Controller
     public function getLanding() {
         return response()->json($this->service->getForLanding());
     }
-
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,14 @@ export default {
             'primary': '#0d91b7',
             'secondary': '#c7eef8',
             'tertiary': '#ebf9ff'
-        }
+        },
+        backgroundSize: {
+            'size-200': '200% 200%',
+        },
+        backgroundPosition: {
+            'pos-0': '0% 0%',
+            'pos-100': '100% 0%',
+        },
     },
   },
   plugins: [
