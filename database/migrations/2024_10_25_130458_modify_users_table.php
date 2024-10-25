88b322f83872257344c8fda6a0f16d491722f218
deethesaint <vtduong0912@gmail.com>
2024-10-25 13:06:37 +0700

    fix: add down() migration

--- database/migrations/2024_10_25_130458_modify_users_table.php
+++ database/migrations/2024_10_25_130458_modify_users_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
     }
 };
