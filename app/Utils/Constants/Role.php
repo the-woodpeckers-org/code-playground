65d6f2396889c3e4e81dd6f416ff4bce82f09283
deethesaint <vtduong0912@gmail.com>
2024-10-25 13:03:29 +0700

    update: add constants role class

---
+++ app/Utils/Constants/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Utils\Constants;
+
+class Role
+{
+    const SuperAdmin = "super_admin";
+    const Admin = "admin";
+    const Contributor = "contributor";
+    const User = "user";
+}
