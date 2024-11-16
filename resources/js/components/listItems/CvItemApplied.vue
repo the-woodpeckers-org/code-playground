93fc440cbd7bc51f373129eae37ba02be41113cc
Bjkdy <pv198357@gmail.com>
2024-11-17 00:04:57 +0700

    show list cv apply for the job

---
+++ resources/js/components/listItems/CvItemApplied.vue
@@ -0,0 +1,45 @@
+<template>
+    <tr>
+        <td>{{ formattedTime }}</td>
+        <td>{{ name }}</td>
+        <td><a :href="linkCV" target="_blank">{{ linkCV }}</a></td>
+        <td></td>
+        <td></td>
+    </tr>
+</template>
+<script>
+export default {
+    name: "CvItemApplied",
+    props: {
+        name: {
+            type: String,
+            default: 'N/A'
+        },
+        time: {
+            type: String,
+            default: 'N/A'
+        },
+        id_cv: {
+            type: String,
+            default: 0
+        },
+        linkCV: {
+            type: String,
+            default: 'N/A'
+        },
+    },
+    computed: {
+        formattedTime() {
+            if (this.time === 'N/A') return 'N/A';
+            const date = new Date(this.time);
+            return date.toLocaleString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+            });
+        }
+    }
+}
+</script>
