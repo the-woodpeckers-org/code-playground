3524b485d689c4c179f6fa52b1cf0978d13d40df
deethesaint <vtduong0912@gmail.com>
2024-11-22 02:49:05 +0700

    update: toast edit

--- resources/js/components/messages/Toast.vue
+++ resources/js/components/messages/Toast.vue
@@ -12,13 +12,13 @@ export default {
     mounted() {
         setTimeout(() => {
             this.isHidden = true;
-        }, 4000)
+        }, 5000)
     }
 }
 </script>
 
 <template>
-    <div class="fixed bottom-5 end-5" :class="{'hidden': isHidden}">
+    <div class="fixed bottom-5 end-5 lobster" :class="{'hidden': isHidden}">
         <div class="alert" :class="{'alert-success':  toastData.type === 'success'}">
             <p class="font-semibold text-base-200">{{ toastData.message }}!</p>
         </div>
@@ -26,5 +26,31 @@ export default {
 </template>
 
 <style scoped>
+@keyframes fade {
+    0% {
+        opacity: 0;
+    }
+    10% {
+        opacity: 1
+    }
+    25% {
+        opacity: 1;
+    }
+    50% {
+        opacity: 1;
+    }
+    95% {
+        opacity: 1;
+    }
+    100% {
+        display: none;
+        opacity: 0;
+    }
+}
 
+.lobster {
+    animation-name: fade;
+    animation-duration: 5000ms;
+    animation-fill-mode: forwards;
+}
 </style>
--- resources/js/components/pages/Contributor/Problem/ProblemManager.vue
+++ resources/js/components/pages/Contributor/Problem/ProblemManager.vue
@@ -1,15 +1,19 @@
 <script>
 import {HTTP} from "@/http-common.js";
 import CreateProblem from "@/components/pages/Contributor/Problem/CreateProblem.vue";
+import Toast from "@/components/messages/Toast.vue";
 
 export default {
     name: "ProblemManager",
-    components: {CreateProblem},
+    components: {Toast, CreateProblem},
     data: () => {
         return {
             currentPage: '1',
             problems: Array,
-            links: Array
+            links: Array,
+            deletingTitle: '',
+            deletingId: '',
+            isDeletedSomeRecord: false,
         }
     },
     mounted() {
@@ -28,6 +32,24 @@ export default {
                 .catch((err) => {
                     console.log(err);
                 })
+        },
+        async getProblem(id) {
+
+        },
+        showConfirmDelete(deletingId, deletingTitle) {
+            this.isDeletedSomeRecord = false;
+            this.deletingId = deletingId;
+            this.deletingTitle = deletingTitle;
+            document.getElementById("modal_delete").classList.add('modal-open')
+        },
+        confirmNo() {
+            this.deletingId = this.deletingTitle = '';
+            document.getElementById("modal_delete").classList.remove('modal-open')
+        },
+        async confirmDelete() {
+            let _this = this;
+            this.isDeletedSomeRecord = true;
+            this.confirmNo();
         }
     }
 }
@@ -55,23 +77,41 @@ export default {
                 <tbody>
                 <tr class="hover" v-for="problem in problems">
                     <th></th>
-                    <td>{{ problem.title}}</td>
-                    <td>{{ problem.difficulty}}</td>
-                    <td>{{ problem.acceptance_rate}}</td>
+                    <td>{{ problem.title }}</td>
+                    <td>{{ problem.difficulty }}</td>
+                    <td>{{ problem.acceptance_rate }}</td>
                     <td>
                         <button class="bg-blue-300 px-3 py-1 mx-1 hover:bg-blue-500 rounded-lg">Edit</button>
-                        <button class="bg-red-300 px-3 py-1 mx-1 hover:bg-red-500 rounded-lg">Delete</button>
+                        <button class="bg-red-300 px-3 py-1 mx-1 hover:bg-red-500 rounded-lg"
+                                @click="showConfirmDelete(problem.id, problem.title)">Delete
+                        </button>
                     </td>
                 </tr>
                 </tbody>
             </table>
             <div class="flex">
                 <div class="join mx-auto my-3">
-                    <button v-for="link in links" :disabled="!link.url" class="join-item btn" :class="{ 'btn-active' : link.label == currentPage}" @click="getProblems(link.label)"><span v-html="link.label"></span></button>
+                    <button v-for="link in links" :disabled="!link.url" class="join-item btn"
+                            :class="{ 'btn-active' : link.label == currentPage}" @click="getProblems(link.label)"><span
+                        v-html="link.label"></span></button>
                 </div>
             </div>
         </div>
     </div>
+    <dialog id="modal_delete" class="modal">
+        <div class="modal-box">
+            <h3 class="text-lg font-bold">Warning!</h3>
+            <p class="pt-4 pb-2">Are you sure you want to delete this problem?</p>
+            <p class="pt-2 pb-4"> {{ deletingTitle }}</p>
+            <div class="w-full text-end">
+                <button class="px-3 py-1 bg-blue-300 hover:bg-blue-500 rounded-lg mx-1" @click="confirmDelete">Yes
+                </button>
+                <button class="px-3 py-1 bg-yellow-300 hover:bg-yellow-500 rounded-lg mx-1" @click="confirmNo">No
+                </button>
+            </div>
+        </div>
+    </dialog>
+    <Toast v-if="isDeletedSomeRecord" :toastData="{type: 'success', message: 'Delete problem successfully'}"></Toast>
 </template>
 
 <style scoped>
