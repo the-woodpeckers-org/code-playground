5eabc99550bd76c52f72e5838d5fa2e175a2887f
deethesaint <vtduong0912@gmail.com>
2024-11-21 23:16:36 +0700

    update: contributor components

---
+++ resources/js/components/pages/Contributor/Contest/ContestManager.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "ContestManager"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/pages/Contributor/Course/CourseManager.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "CourseManager"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
