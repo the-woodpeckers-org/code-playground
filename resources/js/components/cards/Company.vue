5d85d8183ef8822d2d90e02267fa078c6aa60fcd
deethesaint <vtduong0912@gmail.com>
2024-12-17 11:21:21 +0700

    fix: flex to grid

--- resources/js/components/cards/Company.vue
+++ resources/js/components/cards/Company.vue
@@ -13,7 +13,7 @@ export default {
 
 <template>
     <router-link :to="url">
-        <div class="border rounded-xl h-52 w-96 my-2 mx-3 p-3 bg-base-100 hover:scale-105 hover:bg-base-200 transition">
+        <div class="border rounded-xl h-52 w-auto my-2 mx-3 p-3 bg-base-100 hover:scale-105 hover:bg-base-200 transition">
             <img class="w-20 h-20 object-center object-contain" :src="imgUrl" loading="lazy">
             <h1 class="text-xl font-semibold">{{ title }}</h1>
             <p class="text-ellipsis line-clamp-1" v-html="description"></p>
